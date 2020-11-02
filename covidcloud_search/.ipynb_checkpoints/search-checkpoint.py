22d357b23c955182c44ceb4fd0efbb8c753a7987
aymanalbaz <ayman@dnastack.com>
2020-11-02 10:57:46 -0500

    made search more user friendly

--- covidcloud_search/.ipynb_checkpoints/search-checkpoint.py
+++ covidcloud_search/.ipynb_checkpoints/search-checkpoint.py
@@ -4,33 +4,29 @@ import requests
 from typing import List, Union
 
 
-default_drs_url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/objects/'
-default_search_url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/search'
-
 def ga4gh_get_drs(
-    query: str = '',
-    url: str = default_drs_url,
-) -> Union[List[dict], dict]:
+    id: str = '',
+) -> Union[List[dict], dict, int]:
     
     """
     Get DRS from DNAstack's covidcloud
     
-    :param query: DRS ID. Leave '' to get all drs data.
-    :param url: API url
+    :param id: DRS ID. Leave '' to get all drs data.
 
-    :return: Json response/s in dict format
+    :return:
+        If statuscode 200: Json response/s in dict format
+        If statuscode 400: Returns statuscode
 
     Example:
-    url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/objects/'
-    query = '0000bee7-6e47-4e0c-b3e4-9e574eac508b'
-    drs_data  = ga4gh_get_drs(query, url)
+        id = '0000bee7-6e47-4e0c-b3e4-9e574eac508b'
+        drs_data  = ga4gh_get_drs(id)
     """
-    
-    extended_url = os.path.join(url, query)
+    drs_url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/objects/'
+    extended_url = os.path.join(drs_url, id)
     response = requests.get(extended_url)
     
     if response.status_code == 200:
-        if query:
+        if id:
             return response.json()['object']
         else:
             return response.json()['objects']
@@ -41,25 +37,25 @@ def ga4gh_get_drs(
 
 def ga4gh_search_db(
     query: str,
-    url: str = default_search_url
-) -> List[dict]:
+) -> Union[List[dict], int]:
 
     """
     Returns list formatted ga4gh search response from a url and query
 
     :param query: SQL query
-    :param url: API url
     
-    :return: Json response/s in dict format
+    :return: 
+        If statuscode 200: Json response/s in dict format
+        If statuscode 400: Returns statuscode
 
     Example:
-    url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/search'
-    query = {'query': 'SELECT * FROM coronavirus_dnastack_curated.covid_cloud_production.sequences_view'}
-    meta_data  = ga4gh_search_db(query, url)
+        query = 'SELECT * FROM coronavirus_dnastack_curated.covid_cloud_production.sequences_view LIMIT 1000'
+        meta_data  = ga4gh_search_db(query)
 
     """
 
-    response = requests.post(url, json=query)
+    search_url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/search'
+    response = requests.post(url, json={'query': query})
     
     if response.status_code == 200:
         response_url = response.json()['pagination']
