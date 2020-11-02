9dcd42dbf1c6dc3cce8696fcf05ac00c4c10ed78
aymanalbaz <ayman@dnastack.com>
2020-11-02 11:15:00 -0500

    re-added library files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='covidcloud_search',
+    packages=find_packages(include=['covidcloud_search']),
+    version='0.1.0',
+    description='DNAstack CovidCloud Search Library',
+    author='Ayman A',
+    license='MIT',
+    install_requires=['requests==2.22.0'],
+)
