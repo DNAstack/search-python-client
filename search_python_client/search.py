39b73a57aac146fb2d064ce70f4d7ce246c0e880
aymanalbaz <ayman@dnastack.com>
2020-11-10 16:56:34 -0500

    [#175673141] Added row based pagination

--- search_python_client/search.py
+++ search_python_client/search.py
@@ -2,30 +2,30 @@ import os
 import pandas as pd
 import requests
 
-from typing import List
+from typing import Iterator
 
 
 class DrsClient:
     """
     DRS client for DNAstack's DRS API's.
 
-    :param url: Base url to search on
+    :param base_url: Base url to search on
 
     :Example:
         from search_python_client.search import DrsClient\n
-        url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/'\n
-        drs_client = DrsClient(url=url)
+        base_url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/'\n
+        drs_client = DrsClient(base_url=base_url)
 
     """
 
-    def __init__(self, url: str):
-        self.url = url
+    def __init__(self, base_url: str):
+        self.base_url = base_url
 
     def __str__(self):
-        return f'DrsClient(url={self.url})'
+        return f'DrsClient(base_url={self.base_url})'
 
     def __repr__(self):
-        return f'DrsClient(url={self.url})'
+        return f'DrsClient(base_url={self.base_url})'
 
     def _get(self, url: str) -> dict:
         """
@@ -40,7 +40,7 @@ class DrsClient:
         response.raise_for_status()
         return response.json()
 
-    def object_info(self, object_id: str) -> pd.DataFrame:
+    def get_object_info(self, object_id: str) -> pd.DataFrame:
         """
         List all info associated with object.
 
@@ -50,7 +50,7 @@ class DrsClient:
 
         """
         return pd.DataFrame(
-            self._get(os.path.join(self.url, 'objects', object_id))
+            self._get(os.path.join(self.base_url, 'objects', object_id))
         )
 
 
@@ -58,23 +58,23 @@ class SearchClient:
     """
     Search client for DNAstack's search API's.
 
-    :param url: Base url to search on
+    :param base_url: Base url to search on
 
     :Example:
         from search_python_client.search import SearchClient\n
-        url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/'\n
-        search_client = SearchClient(url=url)
+        base_url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/'\n
+        search_client = SearchClient(base_url=base_url)
 
     """
 
-    def __init__(self, url: str):
-        self.url = url
+    def __init__(self, base_url: str):
+        self.base_url = base_url
 
     def __str__(self):
-        return f'SearchClient(url={self.url})'
+        return f'SearchClient(base_url={self.base_url})'
 
     def __repr__(self):
-        return f'SearchClient(url={self.url})'
+        return f'SearchClient(base_url={self.base_url})'
 
     def _get(self, url: str) -> dict:
         """
@@ -89,33 +89,27 @@ class SearchClient:
         response.raise_for_status()
         return response.json()
 
-    def _get_paginated(self, url: str, pages: int) -> List[dict]:
+    def _get_paginated(self, url: str) -> Iterator:
         """
         Executes get request for paginated responses
 
         :param url: Url to search on
-        :param pages: Number of pages to return
         :returns: List of dict formatted json responses
         :raises HTTPError: If response != 200
 
         """
-        base_name = os.path.basename(url)
-        json_responses = []
+        basename = os.path.basename(url)
         json_response = self._get(url)
-        json_responses += json_response[base_name]
-
-        pages_counter = 1
-        while 'pagination' in json_response and pages_counter < pages:
+        while True:
             try:
                 json_response = self._get(
                     json_response['pagination']['next_page_url']
-                    )
+                )
             except KeyError:
-                return json_responses
-            json_responses += json_response[base_name]
-            pages_counter += 1
-
-        return json_responses
+                break
+            if json_response[basename]:
+                for row in json_response[basename]:
+                    yield row
 
     def _post(self, url: str, json: dict) -> dict:
         """
@@ -131,52 +125,43 @@ class SearchClient:
         response.raise_for_status()
         return response.json()
 
-    def _post_paginated(self, url, json, pages):
+    def _post_paginated(self, url, json) -> Iterator:
         """
         Executes post request for paginated responses
 
         :param url: Url to search on
         :param json:  Dict formatted json query
-        :param pages: Number of pages to return
         :returns: List of dict formmated json responses
         :raises HTTPError: If response != 200
 
         """
-        json_responses = []
         json_response = self._post(url, json=json)
-        json_responses += json_response['data']
-
-        pages_counter = 1
-        while 'pagination' in json_response and pages_counter < pages:
+        while True:
             try:
                 json_response = self._get(
                     json_response['pagination']['next_page_url']
-                    )
+                )
             except KeyError:
-                return json_responses
-            json_responses += json_response['data']
-            pages_counter += 1
+                break
+            if json_response['data']:
+                for row in json_response['data']:
+                    yield row
 
-        return json_responses
-
-    def list_tables(self, pages: int = 100) -> pd.DataFrame:
+    def get_table_list(self) -> Iterator:
         """
         List all tables associated with the url.
 
-        :param pages: Number of pages to return. Defaults to 100
         :returns: Dataframe formatted responses
         :raises HTTPError: If response != 200
 
         """
-        return pd.DataFrame(
-            self._get_paginated(
-                os.path.join(self.url, 'tables'), pages
-                )
+        return self._get_paginated(
+            os.path.join(self.base_url, 'tables')
         )
 
-    def table_info(self, table_name: str) -> pd.DataFrame:
+    def get_table_info(self, table_name: str) -> pd.DataFrame:
         """
-        List all info associated with table_name.
+        get all info associated with table_name.
 
         :param table_name: Table name of choice
         :returns: Dataframe formatted responses
@@ -184,39 +169,33 @@ class SearchClient:
 
         """
         return pd.DataFrame(
-            self._get(os.path.join(self.url, 'table', table_name, 'info'))
+            self._get(os.path.join(self.base_url, 'table', table_name, 'info'))
         )
 
-    def table_data(self, table_name: str, pages: int = 100) -> pd.DataFrame:
+    def get_table_data(self, table_name: str) -> Iterator:
         """
         Get all data associated with table_name.
 
         :param table_name: Table name of choice
-        :param pages: Number of pages to return. Defaults to 100
         :returns: Dataframe formatted responses
         :raises HTTPError: If response != 200
 
         """
-        return pd.DataFrame(
-            self._get_paginated(
-                os.path.join(self.url, 'table', table_name, 'data'), pages
-                )
+        return self._get_paginated(
+            os.path.join(self.base_url, 'table', table_name, 'data')
         )
 
-    def search_table(self, query: str, pages: int = 100) -> pd.DataFrame:
+    def search_table(self, query: str) -> Iterator:
         """
         Executes an SQL query on table of choice and returns associated data.
 
         :param table_name: Table name of choice
-        :param pages: Number of pages to return. Defaults to 100
         :returns: Dataframe formatted responses
         :raises HTTPError: If response != 200
 
         :Example: query = 'SELECT * FROM coronavirus_dnastack_curated.covid_cloud_production.sequences_view LIMIT 1000'
 
         """
-        return pd.DataFrame(
-            self._post_paginated(
-                os.path.join(self.url, 'search'), query, pages
-            )
+        return self._post_paginated(
+                os.path.join(self.base_url, 'search'), query
         )
--- tests/test_search.py
+++ tests/test_search.py
@@ -5,39 +5,45 @@ from search_python_client.search import DrsClient, SearchClient
 
 class TestDrsClient(unittest.TestCase):
     def setUp(self):
-        url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/'
-        self.drs_client = DrsClient(url)
+        base_url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/'
+        self.drs_client = DrsClient(base_url)
 
-    def test_object_info(self):
+    def test_get_object_info(self):
         object_id = '0000bee7-6e47-4e0c-b3e4-9e574eac508b'
-        object_info = self.drs_client.object_info(object_id)
+        object_info = self.drs_client.get_object_info(object_id)
         self.assertEqual(object_info.loc['id', 'object'], object_id)
 
 
 class TestSearchClient(unittest.TestCase):
     def setUp(self):
-        url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/'
-        self.search_client = SearchClient(url=url)
+        base_url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/'
+        self.search_client = SearchClient(base_url=base_url)
 
-    def test_list_tables(self):
-        tables = self.search_client.list_tables()
-        self.assertGreaterEqual(len(tables), 1)
+    def test_get_table_list(self):
+        tables_iterator = self.search_client.get_table_list()
+        tables = [next(tables_iterator, None) for i in range(10)]
+        tables = list(filter(None, tables))
+        self.assertEqual(len(tables), 10)
 
-    def test_table_info(self):
+    def test_get_table_info(self):
         table_name = 'coronavirus_dnastack_curated.coronavirus.annotations'
-        table_info = self.search_client.table_info(table_name)
+        table_info = self.search_client.get_table_info(table_name)
         self.assertEqual(table_info.loc['description', 'name'], table_name)
 
-    def test_table_data(self):
+    def test_get_table_data(self):
         table_name = 'coronavirus_dnastack_curated.coronavirus.annotations'
-        table_data = self.search_client.table_data(table_name, 10)
-        self.assertGreaterEqual(len(table_data), 1)
+        table_data_iterator = self.search_client.get_table_data(table_name)
+        table_data = [next(table_data_iterator, None) for i in range(10)]
+        table_data = list(filter(None, table_data))
+        self.assertEqual(len(table_data), 10)
 
     def test_search_table(self):
         table_name = 'coronavirus_dnastack_curated.coronavirus.annotations'
         query = f'SELECT * FROM {table_name} LIMIT 100'
-        table_data = self.search_client.search_table(query)
-        self.assertGreaterEqual(len(table_data), 1)
+        table_data_iterator = self.search_client.search_table(query)
+        table_data = [next(table_data_iterator, None) for i in range(10)]
+        table_data = list(filter(None, table_data))
+        self.assertEqual(len(table_data), 10)
 
 
 if __name__ == '__main__':
