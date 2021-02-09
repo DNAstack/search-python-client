513c04c33ba89353ef7626183ccc1ca360549e0f
aymanalbaz <ayman@dnastack.com>
2021-02-09 16:18:35 -0500

    fixed .get bug

--- search_python_client/search.py
+++ search_python_client/search.py
@@ -1,29 +1,64 @@
+import configparser
 import os
 import pandas as pd
 import requests
 
-from typing import Iterator, Optional
+from typing import Iterator, Optional, Tuple, Union
 
 
 class DrsClient:
     """
     DRS client for DNAstack's DRS API's.
-
     :param base_url: Base url to search on
-    :param username: Username for drs server. Defaults to None
-    :param password: Password for drs server. Defaults to None
-
+    :param auth: (username, pass) for auth access. Defaults to None
+    :param wallet: Token for wallet access . Defaults to None
     :Example:
         from search_python_client.search import DrsClient\n
         base_url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/'\n
         drs_client = DrsClient(base_url=base_url)
-
     """
-
-    def __init__(self, base_url: str, username: Optional[str] = None, password: Optional[str] = None):
+    def __init__(self,
+                 base_url: str,
+                 auth: Optional[Tuple[str, str]] = None,
+                 wallet: Optional[str] = None,
+                 config_file: Optional[Union[str, bytes, os.PathLike]] = None):
         self.base_url = base_url
-        self.username = username
-        self.password = password
+        self.auth = auth
+        self.wallet = wallet
+        self.config_file = config_file
+        self.server_type = None
+        self._set_server_type()
+
+    def _check_valid(self, atr):
+        return atr is not None
+
+    def _check_conflicts(self):
+        # Check that there is only one type of authentication method
+        if sum(map(self._check_valid, [self.auth, self.wallet])) > 1:
+            raise ValueError('Please choose only one authentication method, user authentication or wallet authentication')
+
+    def _read_config_file(self) -> dict:
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
+        return config
+
+    def _config_file_to_atr(self):
+        config = self._read_config_file()
+        self.auth = config.get('drs-auth', 'username', fallback=None), \
+                    config.get('drs-auth', 'password', fallback=None)
+        self.wallet = config.get('drs-wallet', 'token', fallback=None)
+        self._check_conflicts()
+
+    def _set_server_type(self):
+        self._check_conflicts()
+        if self.config_file:
+            self._config_file_to_atr()
+
+        if self.auth:
+            self.server_type = 'auth'
+
+        elif self.wallet:
+            self.server_type = 'wallet'
 
     def __str__(self):
         return f'DrsClient(base_url={self.base_url})'
@@ -34,24 +69,29 @@ class DrsClient:
     def _get(self, url: str) -> dict:
         """
         Executes get request and returns dict formatted json response
-
         :param url: Url to search on
         :returns: dict formatted json response
         :raises HTTPError: If response != 200
-
         """
-        response = requests.get(url, auth=(self.username, self.password))
+        if self.server_type == 'auth':
+            response = requests.get(url, auth=self.auth)
+
+        elif self.server_type == 'wallet':
+            headers = {'Authorization' : f'Bearer {self.wallet}'}
+            response = requests.get(url, headers=headers)
+
+        else:
+            response = requests.get(url)
+
         response.raise_for_status()
         return response.json()
 
     def get_object_info(self, object_id: str) -> pd.DataFrame:
         """
         List all info associated with object.
-
         :param object_info: Object id of choice
         :returns: Dataframe formatted responses
         :raises HTTPError: If response != 200
-
         """
         return pd.DataFrame.from_dict(
             self._get(os.path.join(self.base_url, 'objects', object_id).replace('\\', '/')),
@@ -67,7 +107,7 @@ class SearchClient:
 
     :Example:
         from search_python_client.search import SearchClient\n
-        base_url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/'\n
+        base_url = 'https://ga4gh-search-adapter-presto-covid19-public.staging.dnastack.com'\n
         search_client = SearchClient(base_url=base_url)
 
     """
@@ -105,19 +145,17 @@ class SearchClient:
         """
         basename = os.path.basename(url)
         json_response = self._get(url)
-        if json_response[basename]:
+        if json_response.get(basename, None):
             for row in json_response[basename]:
                 yield row
         while True:
             try:
-                json_response = self._get(
-                    json_response['pagination']['next_page_url']
-                )
+                json_response = self._get(json_response['pagination']['next_page_url'])
+                if json_response.get(basename, None):
+                    for row in json_response[basename]:
+                        yield row
             except (KeyError, TypeError):
                 break
-            if json_response[basename]:
-                for row in json_response[basename]:
-                    yield row
 
     def _post(self, url: str, json: dict) -> dict:
         """
@@ -144,19 +182,17 @@ class SearchClient:
 
         """
         json_response = self._post(url, json=json)
-        if json_response['data']:
+        if json_response.get('data', None):
             for row in json_response['data']:
                 yield row
         while True:
             try:
-                json_response = self._get(
-                    json_response['pagination']['next_page_url']
-                )
+                json_response = self._get(json_response['pagination']['next_page_url'])
+                if json_response.get('data', None):
+                    for row in json_response['data']:
+                        yield row
             except KeyError:
                 break
-            if json_response['data']:
-                for row in json_response['data']:
-                    yield row
 
     def get_table_list(self) -> Iterator:
         """
@@ -204,9 +240,9 @@ class SearchClient:
         :returns: Dataframe formatted responses
         :raises HTTPError: If response != 200
 
-        :Example: query = 'SELECT * FROM coronavirus_dnastack_curated.covid_cloud_production.sequences_view LIMIT 1000'
+        :Example: query = 'SELECT * FROM coronavirus_dnastack_curated.covid_cloud_staging.annotations LIMIT 1000'
 
         """
         return self._post_paginated(
-                os.path.join(self.base_url, 'search').replace('\\', '/'), query
+            os.path.join(self.base_url, 'search').replace('\\', '/'), query
         )
--- tests/test_search.py
+++ tests/test_search.py
@@ -5,10 +5,22 @@ from search_python_client.search import DrsClient, SearchClient
 
 class TestDrsClient(unittest.TestCase):
     def setUp(self):
-        base_url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/'
-        self.drs_client = DrsClient(base_url)
+        self.base_url = 'https://drs.covidcloud.ca/ga4gh/drs/v1/'
 
     def test_get_object_info(self):
+        self.drs_client = DrsClient(self.base_url)
+        object_id = '0000bee7-6e47-4e0c-b3e4-9e574eac508b'
+        object_info = self.drs_client.get_object_info(object_id)
+        self.assertEqual(object_info.id.values[0], object_id)
+
+    def test_get_object_info_auth(self):
+        self.drs_client = DrsClient(self.base_url, auth=('user', 'pass'))
+        object_id = '0000bee7-6e47-4e0c-b3e4-9e574eac508b'
+        object_info = self.drs_client.get_object_info(object_id)
+        self.assertEqual(object_info.id.values[0], object_id)
+
+    def test_get_object_info_wallet(self):
+        self.drs_client = DrsClient(self.base_url, wallet=123)
         object_id = '0000bee7-6e47-4e0c-b3e4-9e574eac508b'
         object_info = self.drs_client.get_object_info(object_id)
         self.assertEqual(object_info.id.values[0], object_id)
@@ -16,7 +28,7 @@ class TestDrsClient(unittest.TestCase):
 
 class TestSearchClient(unittest.TestCase):
     def setUp(self):
-        base_url = 'https://ga4gh-search-adapter-presto-covid19-public.prod.dnastack.com/'
+        base_url = 'https://ga4gh-search-adapter-presto-covid19-public.staging.dnastack.com'
         self.search_client = SearchClient(base_url=base_url)
 
     def test_get_table_list(self):
@@ -26,19 +38,19 @@ class TestSearchClient(unittest.TestCase):
         self.assertEqual(len(tables), 10)
 
     def test_get_table_info(self):
-        table_name = 'coronavirus_dnastack_curated.coronavirus.annotations'
+        table_name = 'coronavirus_dnastack_curated.covid_cloud_staging.annotations'
         table_info = self.search_client.get_table_info(table_name)
         self.assertEqual(table_info.loc['description', 'name'], table_name)
 
     def test_get_table_data(self):
-        table_name = 'coronavirus_dnastack_curated.coronavirus.annotations'
+        table_name = 'coronavirus_dnastack_curated.covid_cloud_staging.annotations'
         table_data_iterator = self.search_client.get_table_data(table_name)
         table_data = [next(table_data_iterator, None) for i in range(10)]
         table_data = list(filter(None, table_data))
         self.assertEqual(len(table_data), 10)
 
     def test_search_table(self):
-        table_name = 'coronavirus_dnastack_curated.coronavirus.annotations'
+        table_name = 'coronavirus_dnastack_curated.covid_cloud_staging.annotations'
         query = f'SELECT * FROM {table_name} LIMIT 100'
         table_data_iterator = self.search_client.search_table(query)
         table_data = [next(table_data_iterator, None) for i in range(10)]
